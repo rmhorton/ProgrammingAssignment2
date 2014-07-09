6bbfaedacc0d2171d7031ffc71ea1440e86282f7
rmhorton <rmhorton@attotron.com>
2014-07-08 19:31:59 -0700

    This is a simple test file to see how the cachematrix object and method work.

---
+++ test_cachematrix.R
@@ -0,0 +1,18 @@
+source("cachematrix.R")
+
+set.seed(123)
+
+my_cacheMatrix <- makeCacheMatrix( matrix( sample(1:16), nrow=4 ) )
+
+my_cacheMatrix$getinverse()
+
+Ainv <- cacheSolve(my_cacheMatrix)
+A <- my_cacheMatrix$get
+
+round( A %*% Ainv )
+round( Ainv %*% A )
+
+my_cacheMatrix$getinverse()
+
+cacheSolve(my_cacheMatrix)
+
